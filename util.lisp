12e15bf1ca5b1b39838e14e0326c6ce9238126c8
D. Thompson <thompdump@gmail.com>
2011-05-27 12:32:36 -0700

    Really add utility functions.

---
+++ util.lisp
@@ -0,0 +1,29 @@
+;;; -*- Mode: LISP; Syntax: COMMON-LISP; Package: DXG; Base: 10 -*-
+(in-package :dxg)
+;;;
+;;; util.lisp: misc utility fns
+;;;
+
+;;;
+;;; transition into DCU
+;;; 
+(defun whitespacep (char)
+  (member char '(#\Newline #\Space #\Tab #\Return #\Linefeed #\Page)))
+
+;; almost 10X faster than DCU version
+(defun empty-string-p (str)
+  (do ((n 0 (1+ n)))
+      ((eq n (length str))
+       t)
+    (if (not (whitespacep (elt str n)))
+	(return nil))))
+
+;;;
+;;; DAT-CL-UTILS/shared-string.lisp
+;;;
+(defun noes (x &optional (no-nils-p t))
+  "Return T if X is NIL or if it is an empty string (as defined by EMPTY-STRING-P). If NO-NILS-P is true, return T if X is the string 'NIL'"
+  (or (not x)
+      (empty-string-p x)
+      (if no-nils-p 
+	  (if (and (stringp x) (string= x "NIL")) t))))
