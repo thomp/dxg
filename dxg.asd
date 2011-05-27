cbaa3ebad60e066f80b7f14220ccb336923bd7f8
D. Thompson <thompdump@gmail.com>
2011-05-26 20:14:12 -0700

    Initial commit

---
+++ dxg.asd
@@ -0,0 +1,8 @@
+;;; -*- Mode: LISP; Syntax: COMMON-LISP; Package: DXG; Base: 10 -*-
+;;;
+;;; dxg.asd: the dxg system definition
+;;;
+(defsystem dxg
+    :serial t
+    :components ((:file "packages") 
+		 (:file "xml-generator")))
---
+++ packages.lisp
@@ -0,0 +1,11 @@
+;;; -*- Mode: LISP; Syntax: COMMON-LISP; Package: CL-USER; Base: 10 -*-
+(in-package :cl-user)
+
+(defpackage :dxg			; DAT XML generator
+  (:use :cl) 
+  (:documentation "An XML generator/emitter. The program is released under the terms of the Lisp Lesser GNU Public License http://opensource.franz.com/preamble.html, also known as the LLGPL. Copyright: David A. Thompson, 2006-2011")
+  (:export
+   empty-tag
+   end-tag
+   start-tag
+   xmlc))
