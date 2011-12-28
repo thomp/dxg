2a23ab8e07660e2f20557d3c52493957b85d7500
D Thompson <thompdump@gmail.com>
2011-12-28 08:49:03 -0800

    New comments to reflect code moved into DCU.

--- util.lisp
+++ util.lisp
@@ -5,12 +5,14 @@
 ;;;
 
 ;;;
-;;; transition into DCU
+;;; DCU/shared-char
 ;;; 
 (defun whitespacep (char)
   (member char '(#\Newline #\Space #\Tab #\Return #\Linefeed #\Page)))
 
-;; almost 10X faster than DCU version
+;;;
+;;; DAT-CL-UTILS/shared-string.lisp
+;;;
 (defun empty-string-p (str)
   (do ((n 0 (1+ n)))
       ((eq n (length str))
@@ -18,9 +20,6 @@
     (if (not (whitespacep (elt str n)))
 	(return nil))))
 
-;;;
-;;; DAT-CL-UTILS/shared-string.lisp
-;;;
 (defun noes (x &optional (no-nils-p t))
   "Return T if X is NIL or if it is an empty string (as defined by EMPTY-STRING-P). If NO-NILS-P is true, return T if X is the string 'NIL'"
   (or (not x)
